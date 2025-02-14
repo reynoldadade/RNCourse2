14a491f6aea6acb9d27522e793312e6dd5174177
Reynold <reynoldadade@gmail.com>
2025-02-14 18:38:46 +0100

    first draft background images

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,7 +1,42 @@
-import { TextInput, View, StyleSheet } from 'react-native'
+import { TextInput, View, StyleSheet, Alert } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
+import { useState } from 'react'
+import Colors from '../constants/colors'
 
-export default function StartGameScreen() {
+interface StartGameScreenProps {
+    onPickNumber: (pickedNumber: number) => void
+}
+export default function StartGameScreen({
+    onPickNumber,
+}: StartGameScreenProps) {
+    const [enteredNumber, setEnteredNumber] = useState<string>('')
+
+    function numberInputHandler(enteredText: string) {
+        setEnteredNumber(enteredText)
+    }
+    function resetInputHandler() {
+        setEnteredNumber('')
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber)
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            return Alert.alert(
+                'Invalid Number!',
+                'Number has to be a number between 1 and 99',
+                [
+                    {
+                        text: 'OK',
+                        style: 'destructive',
+                        onPress: resetInputHandler,
+                    },
+                ]
+            )
+            // show alert
+        }
+        onPickNumber(chosenNumber)
+    }
     return (
         <View style={styles.inputContainer}>
             <TextInput
@@ -10,14 +45,20 @@ export default function StartGameScreen() {
                 keyboardType="number-pad"
                 autoCapitalize="none"
                 autoCorrect={false}
+                onChangeText={numberInputHandler}
+                value={enteredNumber}
             />
             {/* Buttons here */}
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHandler}>
+                        Reset
+                    </PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
+                    <PrimaryButton onPress={confirmInputHandler}>
+                        Confirm
+                    </PrimaryButton>
                 </View>
             </View>
         </View>
@@ -30,7 +71,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 16,
         marginTop: 100,
-        backgroundColor: '#4e0329',
+        backgroundColor: Colors.primary800,
         marginHorizontal: 24,
         borderRadius: 8,
         elevation: 4,
@@ -45,9 +86,9 @@ const styles = StyleSheet.create({
     numberInput: {
         height: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb53f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb53f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         width: 50,
